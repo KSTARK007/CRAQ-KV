dac6b80ae37e149e6b1b964349729782a724a292
makneee <makneee@node0.testrdma.rasl-pg0.utah.cloudlab.us>
2024-05-06 13:21:39 -0600

    Lock when deletion

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -923,6 +923,7 @@ void server_worker(
             }
             write_response.is_writing = false;
             // if (write_response.ready(expected_responses))
+            if (true)
             {
               LOG_STATE("Write response ready {} {} {}", k, write_response.remote_index, write_response.remote_port);
               server.put_response(write_response.remote_index, write_response.remote_port, ResponseType::OK);
