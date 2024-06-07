e6e843277f6880698e0d79f5a076201ac69e050c
makneee <makneee@node0.ldc-nodes-2.rasl-pg0.utah.cloudlab.us>
2024-06-07 09:22:26 -0600

    A

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -492,7 +492,7 @@ void server_worker(
   if (thread_index == 0)
   {
     cache->add_callback_on_eviction([&, db, cache, ops_config](const EvictionCallbackData<std::string, std::string>& data){
-      if (data.dirty && config.baseline.one_sided_rdma_enabled)
+      if (data.dirty)
       {
         dirty_entries.enqueue(data);
       }
