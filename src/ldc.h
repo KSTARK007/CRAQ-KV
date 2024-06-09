e1747cf3e9f1547a01cca8de5fb6592d78efadd8
makneee <makneee@node0.write-clusters.rasl-pg0.utah.cloudlab.us>
2024-06-09 15:07:00 -0600

    Access rate

--- src/ldc.h
+++ src/ldc.h
@@ -898,8 +898,8 @@ struct RDMAKeyValueCache : public RDMAData
       writes.fetch_add(1, std::memory_order::relaxed);
     });
     cache->add_callback_on_eviction([this, ops_config](EvictionCallbackData<std::string, std::string> data){
-      LOG_RDMA_DATA("Evicted {}", data.key);
-      snapshot->update_evicted(convert_string<uint64_t>(data.key));
+      // LOG_RDMA_DATA("Evicted {}", data.key);
+      // snapshot->update_evicted(convert_string<uint64_t>(data.key));
       // if (ops_config.use_cache_logs)
       // {
       //   cache_index_logs->append_entry_k(data.key);
@@ -908,9 +908,9 @@ struct RDMAKeyValueCache : public RDMAData
       // {
       //   cache_indexes->dealloc_remote(data.key);
       // }
-      cache_index_eviction_queue.enqueue(data);
-      write_cache_index_cv.notify_one();
-      writes.fetch_add(1, std::memory_order::relaxed);
+      // cache_index_eviction_queue.enqueue(data);
+      // write_cache_index_cv.notify_one();
+      // writes.fetch_add(1, std::memory_order::relaxed);
     });
     LOG_RDMA_DATA("[RDMAKeyValueCache] Initialized");
 
