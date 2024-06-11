d007b9d283865f3764356e0eb90db3b9f00347bd
Maknee <zhu109@illinois.edu>
2024-06-11 15:08:46 -0600

    Con queueuhs

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -791,7 +791,7 @@ void server_worker(
               auto shared_config_port = shared_log_config.port + thread_index;
               LOG_STATE("[PutRequest - shared_log_put_request] Shared log hash {} remote_index {} remote_port {} -> {} {}", hash, remote_index, remote_port, shared_log_config.index, shared_config_port);
               static std::atomic<uint64_t> every_time = 0;
-              if (every_time.fetch_add(1) % 2 == 0)
+              if (every_time.fetch_add(1) % 8 == 0)
               {
                 server.shared_log_put_request(shared_log_config.index, shared_config_port, key_cstr, value_cstr, hash);
               }
