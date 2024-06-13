621d5c102b8ce344294c03f7492454da3d656250
Maknee <zhu109@illinois.edu>
2024-06-12 22:56:17 -0600

    LDCush

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -708,7 +708,7 @@ void server_worker(
   MPMCQueue<LogEntry> unprocessed_log_entries(1024 * 1024);
   if (has_shared_log) {
     server.connect_to_remote_machine(shared_log_config.index);
-    if (thread_index == 0 && machine_index != server_start_index) {
+    if (thread_index == 3 && machine_index != server_start_index) {
       // periodically gets the latest log entries from the shared log, entries not applied yet
       static std::thread background_get_thread([&]() {
         auto print_time = std::chrono::high_resolution_clock::now() + std::chrono::seconds(5);
