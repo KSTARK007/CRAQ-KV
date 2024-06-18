ac571145909e326b65a5302aa45b4eb6196e5b56
Maknee <zhu109@illinois.edu>
2024-06-18 09:16:34 -0600

    A

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -825,7 +825,6 @@ void server_worker(
     cache->add_callback_on_eviction([&, db, cache, ops_config](const EvictionCallbackData<std::string, std::string>& data){
       if (data.dirty)
       {
-        info("ENQUEUE");
         dirty_entries.enqueue(data);
       }
     });
@@ -1863,7 +1862,7 @@ int main(int argc, char *argv[])
         auto diff_cache_hits = current_cache_hits - last_cache_hits;
         auto diff_cache_misses = current_cache_misses - last_cache_misses;
 
-        info("Ops [{}] +[{}] | RDMA [{}] +[{}] | Disk [{}] +[{}] | C Read [{}] +[{}] | C Hit [{}] +[{}] | C Miss [{}] +[{}] | R Disk [{}] +[{}] | L Disk [{}] +[{}] | Writes [{}] +[{}] | Writes Cache [{}] +[{}] | Writes Disk [{}] +[{}]", 
+        info("Ops [{}] +[{}] | RDMA [{}] +[{}] | Disk [{}] +[{}] | C Read [{}] +[{}] | C Hit [{}] +[{}] | C Miss [{}] +[{}] | R Disk [{}] +[{}] | L Disk [{}] +[{}] | Writes [{}] +[{}] | Writes Cache [{}] +[{}]", 
             current_ops_executed, diff_ops_executed,
             current_rdma_executed, diff_rdma_executed,
             current_disk_executed, diff_disk_executed,
