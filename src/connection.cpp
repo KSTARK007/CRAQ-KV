7edf3bf52754c2e9c7a55450fab8f6de0237685c
maknee <zhu109@illinois.edu>
2024-11-25 16:43:38 -0700

    EH

--- src/connection.cpp
+++ src/connection.cpp
@@ -199,7 +199,8 @@ std::string Connection::get(int index, int thread_index, std::string_view key)
       LOG_STATE("[{}-{}:{}] Get response [key = {}, value = {}]", machine_index,
             index, remote_port, key, value);
     } else {
-      panic("Unexpected response: [{}-{}] -> [{}-{}] Get request [{}]", machine_index, index, remote_index, remote_port, data.toString().flatten().cStr());
+      auto string_data = data.toString().flatten();
+      panic("Unexpected response: [{}-{}] -> [{}-{}] Get request [{}]", machine_index, index, remote_index, remote_port, string_data.cStr());
       info("Unexpected response: [{}-{}] -> [{}-{}] Get request [{}]", machine_index, index, remote_index, remote_port, kj::str(data).cStr());
     }
   });
