767998172d041a43b3953a577082da3d37bbc225
makneee <makneee@node0.testrdmaslice.sdcs-pg0.utah.cloudlab.us>
2025-02-18 15:10:51 -0700

    BLOCK

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -1450,6 +1450,7 @@ void server_worker(
                           latest_version = versions.latest_version.load(std::memory_order::relaxed);
                         }
                         // [CRAQ] If our key is lagging behind their version
+                        info("CRAQ IS VERSION??? {} {}", kv.craq_clean_version, latest_version);
                         if (kv.craq_clean_version != KEY_VALUE_PTR_INVALID && latest_version < kv.craq_clean_version)
                         {
                           // Ask the tail node
