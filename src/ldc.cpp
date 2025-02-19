c7df2f48bf4d9490de6d9766098c2bc2df403bbb
makneee <makneee@node0.testrdmaslice.sdcs-pg0.utah.cloudlab.us>
2025-02-19 15:54:09 -0700

    BLOCK

--- src/ldc.cpp
+++ src/ldc.cpp
@@ -1686,7 +1686,7 @@ void server_worker(
 
             auto key_index = convert_string<uint64_t>(key_cstr);
 
-            write_disk(key, value);
+            // write_disk(key, value);
 
             CRAQ_INFO("[CraqForwardPropagateRequest] Got forward propagate request for {}", key);
             // Check if it's tail
